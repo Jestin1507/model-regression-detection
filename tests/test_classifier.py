de0f880d04ca334112055740e4b82ec528794b96
Jestin Thomas <jestinthomas1507@gmail.com>
2026-06-28 17:29:46 +0530

    Fix GitHub Actions workflow

--- tests/test_classifier.py
+++
@@ -1,9 +0,0 @@
-from app.classifier import TicketClassifier
-
-classifier = TicketClassifier()
-
-text = "My payment failed yesterday."
-
-prediction = classifier.classify(text)
-
-print(prediction)
